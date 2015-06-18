7471f44b08f3866cdacb51bbcbe47cce4ff5f784
YOUR NAME <skarthikmca55@gmail.com>
2015-06-18 18:50:16 +0530

    Class Added

---
+++ SimpleApp/SimpleApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleApp
+//
+//  Created by Karthik S on 18/06/15.
+//  Copyright (c) 2015 CompIndia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleApp/SimpleApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SimpleApp
+//
+//  Created by Karthik S on 18/06/15.
+//  Copyright (c) 2015 CompIndia. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
